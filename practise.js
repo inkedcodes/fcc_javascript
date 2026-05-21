d37481677ccba8c76340766cf0375ac419985e5f
i r t i <yra229@gmail.com>
2026-05-21 13:51:37 +0530

    edit 0.1

---
+++ practise.js
@@ -0,0 +1,6 @@
+let num = 34;
+console.log(typeof num);
+
+let isUserLoggedIn = true;
+console.log(typeof isUserLoggedIn);
+
---
+++ trivia_bot.js
@@ -0,0 +1,9 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "cody";
+let botLocation = "a computer";
+let favoriteLanguage = "c++";
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+
+console.log(`My favorite programming language is ${favoriteLanguage}.`)
